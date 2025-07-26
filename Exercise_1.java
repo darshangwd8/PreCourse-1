ba9cc56fdc61d7cfcbc8cd0b6b1cfc066dfd1757
Darshan Shivaram <79953207+darshangwd8@users.noreply.github.com>
2025-07-26 18:29:18 -0400

    Update Exercise_1.java

--- Exercise_1.java
+++ Exercise_1.java
@@ -8,29 +8,52 @@ class Stack {
     boolean isEmpty() 
     { 
         //Write your code here 
+    	return (top==-1);
+    	
     } 
 
-    Stack() 
+    Stack()
     { 
-        //Initialize your constructor 
+        //Initialize your constructor
+    	top=-1;
+    	
     } 
   
     boolean push(int x) 
     { 
-        //Check for stack Overflow
+    	//Check for stack Overflow
+    
+    	if(top+1==MAX) {
+    		System.out.println("stack Overflow");
+    		return false;
+    	}
+    	
         //Write your code here
-    } 
+    	a[++top] = x;
+    	return true;
+     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
+    	if(top==-1) {
+    		System.out.println("Stack Underflow");
+    		return(0);
+    	}
+    	
         //Write your code here
+    	return a[top--];
     } 
   
     int peek() 
     { 
         //Write your code here
-    } 
+    	if(!isEmpty()) {
+    		return 0;
+    	}
+        return a[top];
+   }
+ 
 } 
   
 // Driver code 
